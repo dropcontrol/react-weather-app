e2ff7ebc1fa367d52617937b44dfe437f60434b3
hiroshi yamato <yamato@gmail.com>
2021-06-24 18:26:58 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+
+import Title from "./components/Title";
+import Form from "./components/Form";
+import Results from "./components/Results";
 import './App.css';
 
+import { useState } from "react";
+import axios from "axios";
+
 function App() {
+  const [city, setCity] = useState("");
+  const [results, setResults] = useState({
+    country: "",
+    cityName: "",
+    temperature: "",
+    conditionText: "",
+    icon: ""
+  })
+  const getWeather = (e) => {
+      e.preventDefault();
+      axios.get(`http://api.weatherapi.com/v1/current.json?key=30c298482987442e97e73811212406&q=${city}&aqi=no`)
+      .then(res => {
+        setResults({
+          country: res.data.location.country,
+          cityName: res.data.location.name,
+          temperature: res.data.current.temp_c,
+          conditionText: res.data.current.condition.text,
+          icon: res.data.current.condition.icon
+        })
+      })
+      .catch(err => alert("エラーが発生しました。ページをリロードして、もう一度トライしてください。"));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="container">
+        <Title />
+        <Form setCity={setCity} getWeather={getWeather} />
+        <Results results={results} />
+      </div>
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,12 @@
+// Form.js
+
+const Form = ({ setCity, getWeather }) => {
+    return (
+        <form onSubmit={getWeather}>
+            <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)}/>
+            <button type="submit">Get Weather</button>
+        </form>
+    );
+}
+
+export default Form;
